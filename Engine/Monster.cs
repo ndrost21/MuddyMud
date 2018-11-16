4fa28dd37196a4471bfb47bd728538c9983b98c5
Nick <ndrost@gmail.com>
2018-11-16 11:45:58 -0500

    .10
    https://scottlilly.com/learn-c-by-building-a-simple-rpg-index/lesson-10-1-creating-collections-of-objects/

--- Engine/Monster.cs
+++ Engine/Monster.cs
@@ -13,6 +13,7 @@ namespace Engine
         public int MaximumDamage { get; set; }
         public int RewardExperiencePoints { get; set; }
         public int RewardGold { get; set; }
+        public List<LootItem> LootTable { get; set; }
 
         public Monster(int iD, string name, int maximumDamage, int rewardExperiencePoints, int rewardGold, int maximumHitPoints, int currentHitPoints) : base(maximumHitPoints, currentHitPoints)
         {
@@ -21,6 +22,8 @@ namespace Engine
             MaximumDamage = maximumDamage;
             RewardExperiencePoints = rewardExperiencePoints;
             RewardGold = rewardGold;
+
+            LootTable = new List<LootItem>();
         }
     }
 }
--- Engine/Player.cs
+++ Engine/Player.cs
@@ -11,12 +11,17 @@ namespace Engine
         public int Gold { get; set; }
         public int ExperiencePoints { get; set; }
         public int Level { get; set; }
+        public List<InventoryItem> Inventory { get; set; }
+        public List<PlayerQuest> Quests { get; set; }
 
         public Player(int gold, int experiencePoints, int level, int maximumHitPoints, int currentHitPoints) : base(maximumHitPoints, currentHitPoints)
         {
             Gold = gold;
             ExperiencePoints = experiencePoints;
             Level = level;
+
+            Inventory = new List<InventoryItem>();
+            Quests = new List<PlayerQuest>();
         }
     }
 }
