e1e257a164d481c9897e348f17255b399234e3d8
Nick <ndrost@gmail.com>
2018-11-16 14:12:40 -0500

    .14
    https://scottlilly.com/learn-c-by-building-a-simple-rpg-index/lesson-16-2-refactoring-the-player-movement-function/

--- Engine/Player.cs
+++ Engine/Player.cs
@@ -26,5 +26,116 @@ namespace Engine
             Inventory = new List<InventoryItem>();
             Quests = new List<PlayerQuest>();
         }
+
+        public bool HasRequiredItemToEnterThisLocation(Location location)
+        {
+            if (location.ItemRequiredToEnter == null)
+            {
+                return true;
+            }
+
+            foreach (InventoryItem ii in Inventory)
+            {
+                if (ii.Details.ID == location.ItemRequiredToEnter.ID)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool HasQuest(Quest quest)
+        {
+            foreach (PlayerQuest playerQuest in Quests)
+            {
+                if (playerQuest.Details.ID == quest.ID)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool CompletedQuest(Quest quest) {
+            foreach (PlayerQuest playerQuest in Quests)
+            {
+                if (playerQuest.Details.ID == quest.ID)
+                {
+                    return playerQuest.IsCompleted;
+                }
+            }
+
+            return false;
+        }
+
+        public bool HasAllQuestCompletionItems(Quest quest)
+        {
+            foreach (QuestCompletionItem qci in quest.QuestCompletionItems)
+            {
+                bool foundItemInPlayersInventory = false;
+
+                foreach (InventoryItem ii in Inventory)
+                {
+                    if (ii.Details.ID == qci.Details.ID)
+                    {
+                        foundItemInPlayersInventory = true;
+                        if (ii.Quantity < qci.Quantity)
+                        {
+                            return false;
+                        }
+                    }
+                }
+                if (!foundItemInPlayersInventory)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public void RemoveQuestCompletionItems(Quest quest)
+        {
+            foreach (QuestCompletionItem qci in quest.QuestCompletionItems)
+            {
+                foreach (InventoryItem ii in Inventory)
+                {
+                    if (ii.Details.ID == qci.Details.ID)
+                    {
+                        ii.Quantity -= qci.Quantity;
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void AddItemToInventory(Item itemToAdd)
+        {
+            foreach (InventoryItem ii in Inventory)
+            {
+                if (ii.Details.ID == itemToAdd.ID)
+                {
+                    ii.Quantity++;
+
+                    return;
+                }
+            }
+
+            Inventory.Add(new InventoryItem(itemToAdd, 1));
+        }
+
+        public void MarkQuestCompleted(Quest quest)
+        {
+            foreach (PlayerQuest playerQuest in Quests)
+            {
+                if (playerQuest.Details.ID == quest.ID)
+                {
+                    playerQuest.IsCompleted = true;
+
+                    return;
+                }
+            }
+        }
     }
 }
