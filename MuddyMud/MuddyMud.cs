c749feee5896805e8f3318c0a7816061de410f4c
Nick <ndrost@gmail.com>
2018-11-15 22:15:00 -0500

    .04
    https://scottlilly.com/learn-c-by-building-a-simple-rpg-index/lesson-05-1-creating-objects-from-classes/

--- MuddyMud/MuddyMud.cs
+++ MuddyMud/MuddyMud.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Engine;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,24 @@ namespace MuddyMud
 {
     public partial class MuddyMud : Form
     {
+        private Player _player;
+
         public MuddyMud()
         {
             InitializeComponent();
+
+            _player = new Player();
+
+            _player.CurrentHitPoints = 10;
+            _player.MaximumHitPoints = 10;
+            _player.Gold = 0;
+            _player.ExperiencePoints = 0;
+            _player.Level = 1;
+
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lblExperience.Text = _player.ExperiencePoints.ToString();
+            lblLevel.Text = _player.Level.ToString();
         }
 
         private void MuddyMud_Load(object sender, EventArgs e)
@@ -22,9 +38,5 @@ namespace MuddyMud
 
         }
 
-        private void btnTest_Click(object sender, EventArgs e)
-        {
-            lblGold.Text += 1;
-        }
     }
 }
